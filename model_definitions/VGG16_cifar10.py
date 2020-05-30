6ddbe8f08955dbe0788aed480cb909a87277ffbe
Markus Karner <markus.karner1994@gmail.com>
2020-05-30 16:24:44 +0200

    VGG16 adapted and model evaluate for CIFAR10 corrected

--- model_definitions/VGG16_cifar10.py
+++ model_definitions/VGG16_cifar10.py
@@ -1,20 +1,58 @@
+import tensorflow as tf
 import keras
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization
 from keras.datasets import cifar10
-from tensorflow.keras.applications.vgg16 import VGG16
-import numpy as np
+from keras.applications.vgg16 import VGG16
 import os
+import time
+import matplotlib.pyplot as plt
 
 batch_size = 256
 num_classes = 10
-epochs = 50
-data_augmentation = False
+epochs = 150
 num_predictions = 20
+input_shape = (32, 32, 3)
+multi_gpus = False
 save_dir = os.path.join(os.path.dirname(os.getcwd()), 'tests')
 save_dir = os.path.join(save_dir, 'saved_models')
-model_name = 'alexnet_VGG16_trained_model.h5'
+model_name = 'VGG16_CIFAR10_trained_model.h5'
+
+# initialize optimizer & early stopping
+opt = keras.optimizers.SGD(lr=1e-2, momentum=0.9, decay=1e-2 / epochs)
+callback = keras.callbacks.EarlyStopping(monitor='loss', patience=3, verbose=1)
+
+if multi_gpus:
+    # Create a MirroredStrategy.
+    strategy = tf.distribute.MirroredStrategy()
+    print("Number of devices: {}".format(strategy.num_replicas_in_sync))
+
+    # Open a strategy scope.
+    with strategy.scope():
+        # Everything that creates variables should be under the strategy scope.
+        # In general this is only model construction & `compile()`.
+        model = VGG16(
+            weights=None,
+            include_top=True,
+            classes=num_classes,
+            input_shape=input_shape
+        )
+
+        # Compile the model
+        model.compile(loss=keras.losses.categorical_crossentropy
+                      , optimizer=opt
+                      , metrics=["accuracy"])
+
+else:
+    model = VGG16(
+        weights=None,
+        include_top=True,
+        classes=num_classes,
+        input_shape=input_shape
+    )
+    # Compile the model
+    model.compile(loss=keras.losses.categorical_crossentropy
+                  , optimizer=opt
+                  , metrics=["accuracy"])
+
 
 # The data, split between train and test sets:
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -23,34 +61,26 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 # Convert class vectors to binary class matrices.
+y_test = keras.utils.to_categorical(y_test, num_classes)
 y_train = keras.utils.to_categorical(y_train, num_classes)
-
-model = VGG16(
-    weights=None,
-    include_top=True,
-    classes=num_classes,
-    input_shape=(32, 32, 3)
-)
-
-# Compile the model
-model.compile(loss=keras.losses.categorical_crossentropy
-              , optimizer='SGD'
-              , metrics=["accuracy"])
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
 
-if not data_augmentation:
-    print('Not using data augmentation.')
-    model.fit(x_train,
-              y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              validation_split=0.1,
-              shuffle=True,
-              verbose=1)
+start_time = time.time()
+
+history = model.fit(x_train,
+                    y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    validation_split=0.1,
+                    shuffle=True,
+                    callbacks=[callback],
+                    verbose=1)
+
+print(time.time()-start_time)
 
 # Save model and weights
 if not os.path.isdir(save_dir):
@@ -63,3 +93,25 @@ print('Saved trained model at %s ' % model_path)
 scores = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
+
+# list all data in history
+print(history.history.keys())
+
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('VGG16_CIFAR10_model_accuracy.png')
+plt.clf() # clear plot
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('VGG16_CIFAR10_model_loss.png')
