8b6e9e34def9b19e9a5093338a3d8cb6763ffdb4
Markus Karner <markus.karner1994@gmail.com>
2020-06-22 15:17:12 +0200

    changed ImagePickerCIFAR10.py so that indices are not dulicated in return array
    changes in tests_CIFAR10.py:
    - set_printoptions so that no +e01 and so on is printed
    - fixed multiprocessing issue, add time.sleep(5) so that all processes can start smoothly
    - it was necessary to first pick the images and use the resulting array, otherwise every process picks new images

--- tests/CIFAR10/tests_CIFAR10.py
+++ tests/CIFAR10/tests_CIFAR10.py
@@ -14,11 +14,11 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 num_classes = 10
 sample_size = 200
-max_generations = 2
+max_generations = 100
 multiprocessing = True
 
 save_dir = os.path.join(os.path.dirname(os.getcwd()), 'saved_models')
-model_name = 'keras_cifar10_trained_model.h5'
+model_name = 'improved_keras_cifar10_trained_model.h5'
 
 model = tf.keras.models.load_model(save_dir + '\\' + model_name)
 
@@ -32,7 +32,21 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 #print(image_indices)
 #exit()
 
-all_image_indices = np.array([5717, 5644, 3136])
+all_image_indices = np.array([20,1053,4120,724,1412,4084,2693,8955,2091,1648,51,7303,3145,7156
+,4345,899,7724,7792,897,9919,9556,7912,330,9782,638,7232,1916,3888
+,9471,1134,4181,2923,8440,1115,5169,4074,4589,7,1764,5816,3761,599
+,6965,1438,2536,1270,5397,867,5036,7464,6851,7984,2598,1650,2908,4901
+,2132,6013,6129,4991,8823,7793,7201,3100,9013,4716,7525,6812,4456,3292
+,4212,1697,7764,3681,3022,1842,9355,4075,8821,3295,199,5965,4348,5186
+,6115,8306,7332,6577,6714,726,3453,8540,3177,6502,3213,6480,5783,3333
+,4478,681,3500,1534,82,8991,2188,7587,729,5113,3484,1653,9433,8884
+,6505,7701,1046,891,4451,8917,7773,6113,4047,9885,8119,8362,1116,8489
+,2875,3386,8767,6418,6090,8399,545,8359,916,3687,169,9167,1949,952
+,4022,3239,7870,5391,7098,8353,5114,9621,1213,9108,8429,4312,1024,4368
+,2483,6324,851,1537,5526,2266,1271,5625,3487,9719,7101,5760,8380,4508
+,5142,661,8446,8263,7796,3656,8058,8574,4479,2742,9701,4041,7321,3892
+,1877,2839,7872,5303,1426,4979,6889,2840,6878,7673,4997,3884,8526,1179
+,4963,8096,176,4785])
 
 #function only needed for multi processing
 def f(j):
@@ -63,7 +77,7 @@ start_time = time.time()
 if multiprocessing:
     print('Using multiprocessing')
     if __name__ == '__main__':
-        with Pool(processes=3) as p:
+        with Pool(processes=50) as p:
             p.map(f, all_image_indices)
 
 else:
